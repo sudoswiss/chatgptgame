3ec91e201ac30de05ae2a22c6a3c50925a3478a9
sudoswiss <sudoswiss@gmail.com>
2023-06-14 21:19:15 +0200

    Yay, got it working!

--- Assets/Scripts/BrainAdapter.cs
+++ Assets/Scripts/BrainAdapter.cs
@@ -28,49 +28,62 @@ public class BrainAdapter : MonoBehaviour, BrainCallback
 
         foreach (Ability ability in inherentAbilities)
         {
-            options.Add("[" + index + "]" + ability.ToString());
+            options.Add("[" + index + "] " + ability.ToString());
             abilities.Add(ability);
             index++;
         }
         foreach (InteractiveObject interactiveObject in withinVicinityGameObjects) {
             foreach(Ability ability in interactiveObject.WithinVicinityAbilities) {
-                options.Add("[" + index + "]" + ability.ToString());
+                options.Add("[" + index + "] " + ability.ToString());
                 abilities.Add(ability);
                 index++;
             }
         }
         foreach(InteractiveObject interactiveObject in rightNextToGameObjects) {
             foreach(Ability ability in interactiveObject.RightNextToAbilities) {
-                options.Add("[" + index + "]" + ability.ToString());
+                options.Add("[" + index + "] " + ability.ToString());
                 abilities.Add(ability);
                 index++;
             }
         }
         foreach(InteractiveObject interactiveObject in equippedGameObjects) {
             foreach(Ability ability in interactiveObject.EquippedAbilities) {
-                options.Add("[" + index + "]" + ability.ToString());
+                options.Add("[" + index + "] " + ability.ToString());
                 abilities.Add(ability);
                 index++;
             }
         }
 
-        var promptPreamble = "What action do you want to take? Answer only with the number inside the squre brackets.";
+        var contextPreamble = "Again, imagine you are in a hypothetical virtual world. I want you to answer only with the numbers in the square brackets that are presented to you. Given this, ";
+        var promptPreamble = "What action do you want to take? Answer only with the number inside the square brackets.";
         var optionsString = "";
         foreach(string option in options) {
             optionsString += option;
         }
-        var completePrompt = context + " " + promptPreamble + "\n" + optionsString;
+        var completePrompt = contextPreamble + " " + context + " " + promptPreamble + "\n" + optionsString;
         Debug.Log("Adapter> Send prompt: " + completePrompt);
         brain.SendPrompt(new PromptData(completePrompt, abilities, options));
     }
 
     public void DidReceiveResponse(ResponseData responseData) {
         Debug.Log("Adapter> Did receive response: " + responseData.Response);
+        Debug.Log("Adapter> Possible options: ");
+        foreach(string option in responseData.PossibleOptions)
+        {
+            Debug.Log(">>>Adapter> " + option);
+        }
+        
         // e.g. selectedOption = [1] Turn left
-        var selectedOption = responseData.PossibleOptions.First(option => option.Contains(responseData.Response));
+        var selectedOption = responseData.PossibleOptions.First(option => option.Contains(responseData.Response) || responseData.Response.Contains(option));
+        Debug.Log("Adapter> Selected option" + selectedOption);
+
         // e.g. optionWithoutIndex = Turn left
         var optionWithoutIndex = ExtractOption(selectedOption);
-        var selectedAbility = responseData.PossibleAbilities.First(ability => ability.ToString() == optionWithoutIndex);
+        var selectedAbility = responseData.PossibleAbilities.First(ability =>
+            ability.ToString() == optionWithoutIndex || ability.ToString().Contains(optionWithoutIndex) ||
+            optionWithoutIndex.Contains(ability.ToString())
+        );
+        Debug.Log("Adapter> Selected ability: " + selectedAbility);
 
         if(this.Callback != null && selectedAbility != null) {
             this.Callback.PerformAction(selectedAbility);
